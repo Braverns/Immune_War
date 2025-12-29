3422fc7ea6c214ffeac454cb416c70e76a271407
fatkhulhadi <fatkhulhadi003@gmail.com>
2025-12-29 22:15:33 +0700

    DNA 1

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -84,7 +84,7 @@ func register_enemy_kill(enemy: Node) -> void:
 # 🧬 MUTATION SYSTEM (100 COIN)
 # ==================================================
 var mutation_points := 0
-const MUTATION_THRESHOLD := 100 #syarat coin untuk mutasi
+const MUTATION_THRESHOLD := 1 #syarat coin untuk mutasi
 
 var is_mutation_active := false
 var mutation_ammo := 0
